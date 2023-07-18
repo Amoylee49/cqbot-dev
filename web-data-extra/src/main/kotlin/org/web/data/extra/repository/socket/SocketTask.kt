47acbf77a5bcf8e175995691b85fd2a1bb4f10ac
Amoylee49 <Amoyleeo@qq.com>
2023-07-19 07:26:00 +0800

    cqbot dev big change

--- web-data-extra/src/main/kotlin/org/web/data/extra/repository/socket/SocketTask.kt
+++ web-data-extra/src/main/kotlin/org/web/data/extra/repository/socket/SocketTask.kt
@@ -1,71 +1,70 @@
-package org.web.data.extra.repository.socket
-
-import io.vertx.core.Context
-import io.vertx.core.Promise
-import io.vertx.core.Verticle
-import io.vertx.core.Vertx
-import io.vertx.core.buffer.Buffer
-import org.web.data.extra.repository.CharacterRepository
-
-//@Component
-class SocketTask(repository: CharacterRepository) : Verticle {
-//    constructor()
-    /**
-     * 如果有bean需要注入则使用【lateinit var】来实现bean的注入
-     */
-//    @Autowired
-    lateinit var repository: CharacterRepository
-
-    init {
-        this.repository = repository
-
-        init(vertx, null)
-        start(Promise.promise())
-    }
-
-//    final var repository: CharacterRepository
-
-    private var sendMessage: String? = null
-
-    override fun getVertx(): Vertx {
-        return Vertx.vertx()
-    }
-
-    //初始化发送数据
-    override fun init(vertx: Vertx, context: Context?) {
-        sendMessage = repository.getAll().toString()
-//        print("init SendMessage$sendMessage")
-    }
-
-    override fun start(startPromise: Promise<Void>?) {
-        println("start Send Message?$sendMessage")
-        val send = Buffer.buffer(sendMessage.toString(), "UTF-8")
-        this.vertx.createDatagramSocket()
-            .send(send, 8888, "localhost")
-//        sendMessage = repository.readCharacterAsStream()
-    }
-
-    override fun stop(stopPromise: Promise<Void>?) {
-        TODO("Not yet implemented")
-    }
-}
-
-
-//class TaskConstratorTest(apple: Apple) {
-//    private val apple: Apple
+//package org.web.data.extra.repository.socket
+//
+//import io.vertx.core.Context
+//import io.vertx.core.Promise
+//import io.vertx.core.Verticle
+//import io.vertx.core.Vertx
+//import io.vertx.core.buffer.Buffer
+//import org.web.data.extra.repository.CharacterRepository
+//
+////@Component
+//class SocketTask(repository: CharacterRepository) : Verticle {
+////    constructor()
+//    /**
+//     * 如果有bean需要注入则使用【lateinit var】来实现bean的注入
+//     */
+////    @Autowired
+//    lateinit var repository: CharacterRepository
 //
 //    init {
-//        this.apple = apple
-//    }
-//}
-
-//fun main() {
+//        this.repository = repository
 //
-//    val scanner = Scanner(System.`in`)
-//    val socketTask = SocketTask()
-//    while (scanner.hasNext()){
-//        socketTask.init(socketTask.vertx,null)
-//        socketTask.start(Promise.promise())
-//        print(scanner.nextLine())
+//        init(vertx, null)
+//        start(Promise.promise())
+//    }
+//
+////    final var repository: CharacterRepository
+//
+//    private var sendMessage: String? = null
+//
+//    override fun getVertx(): Vertx {
+//        return Vertx.vertx()
+//    }
+//
+//    //初始化发送数据
+//    override fun init(vertx: Vertx, context: Context?) {
+//        sendMessage = repository.getAll().toString()
+////        print("init SendMessage$sendMessage")
+//    }
+//
+//    override fun start(startPromise: Promise<Void>?) {
+//        println("start Send Message?$sendMessage")
+//        val send = Buffer.buffer(sendMessage.toString(), "UTF-8")
+//        this.vertx.createDatagramSocket()
+//            .send(send, 8888, "localhost")
+////        sendMessage = repository.readCharacterAsStream()
+//    }
+//
+//    override fun stop(stopPromise: Promise<Void>?) {
 //    }
 //}
+//
+//
+////class TaskConstratorTest(apple: Apple) {
+////    private val apple: Apple
+////
+////    init {
+////        this.apple = apple
+////    }
+////}
+//
+////fun main() {
+////
+////    val scanner = Scanner(System.`in`)
+////    val socketTask = SocketTask()
+////    while (scanner.hasNext()){
+////        socketTask.init(socketTask.vertx,null)
+////        socketTask.start(Promise.promise())
+////        print(scanner.nextLine())
+////    }
+////}

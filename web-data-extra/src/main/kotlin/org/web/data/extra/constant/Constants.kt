f125c9846115fe4820105d90093d6e8ef3213ff6
Amoylee49 <Amoyleeo@qq.com>
2023-07-19 19:47:06 +0800

    修改各个职业

--- web-data-extra/src/main/kotlin/org/web/data/extra/constant/Constants.kt
+++ web-data-extra/src/main/kotlin/org/web/data/extra/constant/Constants.kt
@@ -13,6 +13,6 @@ object Constants {
     const val WIKI_AREA = "灵魂要塞"
     const val WIKI_NORMAL_CONTENT = "克鲁赛德战记战记玩家共建游戏WIKI，无偿提供相关数据、攻略、前沿消息等..."
 
-    //    val CHARACTER_TYPE = arrayOf("剑士", "骑士", "弓手", "猎人", "法师", "祭司")
-    val CHARACTER_TYPE = arrayOf("剑士")
+        val CHARACTER_TYPE = arrayOf("剑士", "骑士", "弓手", "猎人", "法师", "祭司")
+//    val CHARACTER_TYPE = arrayOf("剑士")
 }
